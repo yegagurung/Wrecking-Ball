2900648802da9d9e6da9855f67b11a0755f31478
yegagurung <70418084+yegagurung@users.noreply.github.com>
2020-11-08 20:45:40 -0500

    Add files via upload

---
+++ Ball.js
@@ -0,0 +1,23 @@
+class Ball {
+  constructor(x, y, width, height,angle) {
+    var options = {
+        'frictionAir':0.005,
+        'density':1
+    }
+    this.body = Bodies.rectangle(x, y, width, height, options);
+    this.width = width;
+    this.height = height;
+    
+    World.add(world, this.body);
+  }
+  display(){
+    var pos =this.body.position;
+    var angle = this.body.angle;
+    push();
+    translate(pos.x, pos.y);
+    rotate(angle);
+    fill("black")
+    ellipse(0,0,this.width,this.height) 
+    pop();
+  }
+};
